db197089fc00eb1a109b0ad789bb34629662fa9c
Rai Icasiano <bos_tsip@yahoo.com>
2020-09-22 18:46:00 +0800

    Update 3.6.2-ensure_default_deny_firewall_policy.sh

--- 3.6.2-ensure_default_deny_firewall_policy.sh
+++ 3.6.2-ensure_default_deny_firewall_policy.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # 3.6.2 Ensure default deny firewall policy
 for rule in `iptables -L | grep -iP '^chain (input|output|forward) \(policy (?!DROP).*\)$' | cut -d" " -f2`; do
   iptables -P $rule DROP
