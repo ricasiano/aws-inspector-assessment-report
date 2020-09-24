1b2d865e990d9f035f3a3cf4bb93b805789b554d
Rai Icasiano <bos_tsip@yahoo.com>
2020-09-24 10:54:47 +0800

    Create 4.1.1.2_ensure_system_is_disabled_when_audit_logs_are_full.sh

---
+++ 4.1.1.2_ensure_system_is_disabled_when_audit_logs_are_full.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# 4.1.1.2 Ensure system is disabled when audit logs are full
+
+if [ `grep -c "^space_left_action" /etc/audit/auditd.conf` -eq 0 ]; then
+  printf "\nspace_left_action = email" >> /etc/audit/auditd.conf
+else
+  sed -Ei "s/^(space_left_action).*=.*/\1 = email/g" /etc/audit/auditd.conf
+fi
+
+if [ `grep -c "^action_mail_acct" /etc/audit/auditd.conf` -eq 0 ]; then
+  printf "\naction_mail_acct = root" >> /etc/audit/auditd.conf
+else
+  sed -Ei "s/^(action_mail_acct).*=.*/\1 = root/g" /etc/audit/auditd.conf
+fi
+
+if [ `grep -c "^admin_space_left_action" /etc/audit/auditd.conf` -eq 0 ]; then
+  printf "\nadmin_space_left_action = halt" >> /etc/audit/auditd.conf
+else
+  sed -Ei "s/^(admin_space_left_action).*=.*/\1 = halt/g" /etc/audit/auditd.conf
+fi
